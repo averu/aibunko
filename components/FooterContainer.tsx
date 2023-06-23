b69a4c66d01a3d5ab606aebabc2102ff36cf9fb3
averu <neuf-seize@outlook.jp>
2023-06-24 01:49:58 +0900

    🐞 fix: 静的解析対応

--- components/FooterContainer.tsx
+++ components/FooterContainer.tsx
@@ -1,5 +1,7 @@
-import { AiFillTwitterCircle } from 'react-icons/ai'
 import Link from 'next/link'
+
+import { AiFillTwitterCircle } from 'react-icons/ai'
+
 import * as styles from '@/components/FooterContainer.css'
 
 const FooterContainer = () => (
