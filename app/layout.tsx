2b4519e8e281f92b762809ac01b002289eb89289
averu <neuf-seize@outlook.jp>
2023-06-25 07:11:25 +0900

    ✨feature: Add metadata and OGP image to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,14 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: '咏曖文庫',
-  description: '',
+  description:
+    'Midjouney(nijijourney)とChatGPTを活用し、装丁デザインの学習を目的とした架空の出版社をイメージしたWebサイトです。',
+  openGraph: {
+    title: '咏曖文庫',
+    description:
+      'Midjouney(nijijourney)とChatGPTを活用し、装丁デザインの学習を目的とした架空の出版社をイメージしたWebサイトです。',
+    images: ['/ogp_image.jpg'],
+  },
 }
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
